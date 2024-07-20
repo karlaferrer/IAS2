673b803f0ec4e62d797dae96d743ed94fb070f6c
karlaferrer <ferreirakarla.kf@gmail.com>
2024-07-20 11:29:41 -0300

    2007

--- IAS2_analise.R
+++ IAS2_analise.R
@@ -8,6 +8,8 @@ pacman::p_load(
   tidyverse,
   tableone, #fazer tabela 1
   gtsummary, # tabelas
+  survival, #analise de sobrevivencia
+  survminer,#plotar graficos ggplot
   readxl, #ler arquivo excel
   descr, #analise descritiva
   kableExtra,#renderizar tabela
@@ -112,5 +114,101 @@ diar_w |>
 
 # 4.Efeitos brutos --------------------------------------------------------
 
+# Eventos ordenados - AG - incrementos independentes
+#risco basal igual para qq evento - historico nao importa
+#acrescenta cluster para corrigir variancia - dado correlacionado
+
+#Efeito bruto - sexo
+ag_sexo <- coxph(Surv(ini, fim, status) ~ sexo + cluster(numcri),data = diar)
+summary(ag_sexo)
+# de acordo com AG nao ha diferenca entre os sexos
+#robust se = 2.37*se(coef)
+#indica que o risco de novos episodios e correlacionado a episodios anteriores
+#o modelo AG nao e indicado,testar PWP e fragilidade
+
+#Efeito bruto - tratamento
+ag_grupo <- coxph(Surv(ini, fim, status) ~ grupo + cluster(numcri),data = diar)
+summary(ag_grupo)
+# segundo AG nao ha diferenca entre os tratamentos
+#robust se > 2*se(coef)
+#o modelo AG nao e indicado,testar PWP e fragilidade
+
+#Efeito bruto - idade
+ag_idade <- coxph(Surv(ini, fim, status) ~ idade + cluster(numcri),data = diar)
+summary(ag_idade)
+#cada mes acrescido na idade da crianca, diminui o risco em 3,3%
+#robust se > 2*se(coef)
+#o modelo AG nao e indicado,testar PWP e fragilidade
+
+#Eventos ordenados estruturados - PWP
+
+#Efeito bruto sexo
+pwp_sexo <- coxph(Surv(ini, fim, status) ~ sexo + cluster(numcri) + strata(enum)
+                  ,data = diar)
+summary(pwp_sexo)
+#nao ha diferenca entre os sexos
+
+a#Efeito bruto tratamento
+pwp_grupo <- coxph(Surv(ini, fim, status) ~ grupo + cluster(numcri) + strata(enum)
+                  ,data = diar)
+summary(pwp_grupo)
+#nao ha diferenca entre os grupos
+
+#Efeito bruto tratamento
+pwp_idade <- coxph(Surv(ini, fim, status) ~ idade + cluster(numcri) + strata(enum)
+                   ,data = diar)
+summary(pwp_idade)
+#cada mes adicional diminui o risco em 1.6%
+
+
+# 5.Efeito do tratamento ajustado -----------------------------------------
+
+#Modelo 1: idade e sexo
+#de importancia teorica sao ajustadas conjuntamente
+pwp_1 <- coxph(Surv(ini, fim, status) ~ idade + sexo + cluster(numcri) + strata(enum)
+               ,data = diar)
+summary(pwp_1)
+
+#Modelo 2: acrescenta o tratamento
+pwp_2 <- coxph(Surv(ini, fim, status) ~ idade + sexo + grupo + cluster(numcri) + strata(enum)
+               ,data = diar)
+summary(pwp_2)
+#tratamento nao tem efeito significativo ajustado por idade e sexo
+
+# nao e possivel comparar os modelos com
+# teste da razao de verossimilhanca 
+#por causa da variancia robusta
+
+
+# 6.Fragilidade -----------------------------------------------------------
+#considerar como fragilidade a media e dejecoes liquidas do episodio anterior
+
+#efeito aleatorio gamma
+pwp_3 <- coxph(Surv(ini, fim, status) ~ idade + sexo + grupo + 
+                 cluster(numcri) + strata(enum) + frailty(episod, sparse = F)
+               ,data = diar)
+summary(pwp_3)
+
+#efeito aleatorio lognormal
+pwp_4 <- coxph(Surv(ini, fim, status) ~ idade + sexo + grupo + 
+                 cluster(numcri) + strata(enum) + 
+                 frailty(episod, sparse = F, dist = "gauss")
+               ,data = diar)
+summary(pwp_4)
+
+#a variancia do efeito alatorio é pequena, mas significativa a 5%
+
+#incluir o numero de dias do episodio anterior
+pwp_5 <- coxph(Surv(ini, fim, status) ~ idade + sexo + grupo + 
+                 cluster(numcri) + strata(enum) + 
+                 frailty(episod, sparse = F) +
+                 frailty(diasant, sparse = F)
+               ,data = diar)
+summary(pwp_5)
+
+#Plot das fragilidades estimadas
+
+
+# 7.Análise de resíduos ---------------------------------------------------
 
 
